016684fbddde9dc9f98b69ed8ec37300b4fec2a8
schmallan <31siyu@gmail.com>
2025-12-15 14:58:29 -0500

    wasd

--- tetris.asm
+++ tetris.asm
@@ -5,14 +5,44 @@
 ;<
 
 section .data
+    title: db "TETRIS",0;
+
     printW: equ 40
     printH: equ 30
     printT: equ printW*printH
-    printgrid: db printT dup '?';
+    printGrid: db printT dup '?';
+
+    cursorX: dq 5
+    cursorY: dq 5
+
+    playX: equ 4
+    playY: equ 4
+    playW: equ 10
+    playH: equ 20
+    playT: equ playW*playH
+    playGrid: db playT dup 0;
+    ;playGrid: db "a000000000b000000000c000000000d000000000e000000000f000000000g000000000h000000000i000000000j000000000k000000000l000000000m000000000n000000000o000000000p000000000q000000000r000000000s000000000t000000000!!!!!!!!!!!!!!!!!!!!!!!!",
     
-    printBuffer: db 30 dup 'x';
+    cyclecount: dq 0
+
+    pieceW: equ 5
+    pieceH: equ 5
+    pieceT: equ pieceW*pieceH
+    pieceGrid: db pieceT dup 2;pieceW dup pieceH dup 0
+    
+    printBuffer: db 50 dup 's';
+
+    colors: db ' ', 176, 177, 178
 
     message: db "helloworld", 0
+
+    blockL: equ 1570
+    blockJ: equ 802
+    blockS: equ 112
+    blockZ: equ 1584
+    blockT: equ 624
+    blockI: equ 240
+    blockO: equ 1632
 
     ;dup as you might guess repeats the expression!!! useful for defining large chunks of stuff.
 ;<
@@ -20,39 +50,139 @@ section .data
 section .text
 global main
 
-main:    
+main:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10*16
+
+    ;mov rcx, cyclecount
+    ;mov [rcx], 0
+
+    mainloop:
+        
+        mov rcx, cyclecount
+        mov rdx, [rcx]
+        inc rdx
+        mov [rcx], rdx
+
+        call wait_
+        call body
+
+
+        mov rcx, 'X'
+        call GetAsyncKeyState
+        cmp al, 0
+    jz mainloop
+
+
+    mov rax, 0
+    mov rsp, rbp;
+    pop rbp;
+ret
+
+wait_:
+    mov rax, 100000000
+    waitloop:
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+    dec rax
+    cmp rax, 0
+    jg waitloop
+ret
+
+body:    
     ;reserve stack space
         push rbp
         mov rbp, rsp
         sub rsp, 32*16
     ;
 
-    ;call drawcanv
+    mov rax, cursorX
+    mov rax, [rax]
+    mov rbx, cursorY
+    mov rbx, [rbx]
+    mov rcx, 0
+    call drawPiece
+    
+    mov rcx, 'W'
+    call GetAsyncKeyState
+    cmp al, 0
+    jz sw
+        mov rax, cursorY
+        mov rbx, [rax]
+        dec rbx
+        mov [rax], rbx
+    sw:
+    mov rcx, 'S'
+    call GetAsyncKeyState
+    cmp al, 0
+    jz ts
+        mov rax, cursorY
+        mov rbx, [rax]
+        inc rbx
+        mov [rax], rbx
+    ts:
+    mov rcx, 'A'
+    call GetAsyncKeyState
+    cmp al, 0
+    jz ta
+        mov rax, cursorX
+        mov rbx, [rax]
+        dec rbx
+        mov [rax], rbx
+    ta:
+    mov rcx, 'D'
+    call GetAsyncKeyState
+    cmp al, 0
+    jz td
+        mov rax, cursorX
+        mov rbx, [rax]
+        inc rbx
+        mov [rax], rbx
+    td:
+    
+    
 
-    ;mov rdx, printgrid
-    ;mov r8, printT
-    ;call print
-    mov r12, 0
-    testl:
-        mov rcx, r12
-        call int2String
-        mov rdx, printBuffer
-        call printConsoleTz
+    call drawcanv
 
-        mov rdx, printBuffer
-        push rdx
-        mov [rdx], ' '
-        inc rdx
-        mov [rdx], r12
-        inc rdx
-        mov [rdx], 10
-        pop rdx
-        mov r8, 3
-        call printConsole
+    mov rax, 5
+    mov rbx, 3
+    mov rdx, title
+    call printCanv
+    
+    mov rcx, cyclecount
+    mov rcx, [rcx]
+    call int2String
+    mov rax, 2
+    mov rbx, 26
+    mov rdx, printBuffer
+    call printCanv
 
-        inc r12
-        cmp r12, 255
-    jng testl
+    mov rax, blockJ
+    call loadPiece
+
+    mov rax, cursorX
+    mov rax, [rax]
+    mov rbx, cursorY
+    mov rbx, [rbx]
+    mov rcx, 3
+    call drawPiece
+
+    call drawPlayGrid
+    
+
+    mov rdx, printGrid
+    mov r8, printT
+    call printConsole
+
 
     ;return stack space
         mov rsp, rbp
@@ -60,7 +190,140 @@ main:
     ;
 ret
 
+drawPiece: ;rabx x, y, rcx col
+    mov r10, pieceGrid
+    add r10, pieceT
+
+    sub rax, 3
+    sub rbx, 3
+
+    mov r9, pieceH
+    dpo:
+        mov r8, pieceW
+        dpi:
+        
+            push rax
+            push rbx
+            push rcx
+            add rax, r8
+            add rbx, r9
+            mov rcx, playW
+            mov rdx, playGrid
+            call calcAdr
+            pop rcx
+
+
+            dec r10
+
+            mov al, [r10]
+            cmp al, 0
+            jz dps
+            mov [rdx], cl
+            dps:
+
+            pop rbx
+            pop rax
+
+        dec r8
+        jg dpi
+    dec r9
+    jg dpo
+ret
+
+loadPiece: ;rax piece
+    mov r8, pieceT
+    lpf:
+    dec r8
+        mov rbx, pieceGrid
+        add rbx, r8
+        mov [rbx], 0
+    cmp r8, 0
+    jg lpf
+    
+    mov r9, 4
+    lpo:
+    dec r9
+        mov r8, 4
+        lpi:
+            dec r8
+
+            push rax
+
+            mov rax, r8
+            mov rbx, r9
+            mov rcx, pieceW
+            mov rdx, pieceGrid
+            call calcAdr
+            mov r10, rdx
+
+            pop rax
+
+            mov [r10], 0
+
+            mov rdx, 0
+            mov rbx, 2
+            div rbx
+            cmp rdx, 0
+            jz lpz
+                mov [r10], 2
+            lpz:
+
+        cmp r8, 0
+        jg lpi
+
+    cmp r9, 0
+    jg lpo
+
+ret
+
 drawcanv:
+    
+    
+    mov r8, 179
+        mov rax, 0 ;vwalls
+        mov rbx, 2
+        mov rcx, printW-1
+        mov rdx, printH-3
+    call fill
+    mov r8, 196
+        mov rax, 1 ;hwalls
+        mov rbx, 1
+        mov rcx, printW-3
+        mov rdx, printH-1
+    call fill
+
+    mov r8, 218
+        mov rax, 0
+        mov rbx, 1
+        mov rcx, 1
+        mov rdx, 1
+    call fill
+    mov r8, 192
+        mov rax, 0
+        mov rbx, printH-1
+        mov rcx, 1
+        mov rdx, 1
+    call fill
+    mov r8, 191
+        mov rax, printW-2
+        mov rbx, 1
+        mov rcx, 1
+        mov rdx, 1   
+    call fill
+    mov r8, 217
+        mov rax, printW-2
+        mov rbx, printH-1
+        mov rcx, 1
+        mov rdx, 1
+    call fill
+
+    
+    mov r8, ' ' ;fill bg
+        mov rax, 1
+        mov rbx, 2
+        mov rcx, printW-3
+        mov rdx, printH-3
+    call fill
     
     mov rax, printW-1 ;fill newlines on right
         mov rbx, 0
@@ -74,14 +337,72 @@ drawcanv:
         mov rdx, 1
         mov r8, 10
     call fill
-    mov rax, 4 ;fill playarea
-        mov rbx, 4
-        mov rcx, 20
-        mov rdx, 20
+
+    mov r8, 179
+        mov rax, playX-1 ;wwalls
+        mov rbx, playY
+        mov rcx, playW*2+2
+        mov rdx, playH
+    call fill
+    mov r8, 196
+        mov rax, playX ;hwalls
+        mov rbx, playY-1
+        mov rcx, playW*2
+        mov rdx, playH+2
+    call fill
+
+
+    mov r8, 218
+        mov rax, playX-1
+        mov rbx, playY-1
+        mov rcx, 1
+        mov rdx, 1
+    call fill
+    mov r8, 192
+        mov rax, playX-1
+        mov rbx, playY+playW*2
+        mov rcx, 1
+        mov rdx, 1
+    call fill
+    mov r8, 191
+        mov rax, playX+playW*2
+        mov rbx, playY-1
+        mov rcx, 1
+        mov rdx, 1
+    call fill
+    mov r8, 217
+        mov rax, playX+playW*2
+        mov rbx, playY+playW*2
+        mov rcx, 1
+        mov rdx, 1
+    call fill
+
+    mov rax, playX ;fill playarea
+        mov rbx, playY
+        mov rcx, playW*2
+        mov rdx, playH
         mov r8, ' '
     call fill
-    
 
+ret
+
+printCanv: ;rdx msg in, rax rbx x y, 
+    push rdx
+    call calcTz
+
+    mov rcx, printW
+    mov rdx, printGrid
+    call calcAdr
+    pop r9
+    ;rdx buffer loc, r8 print#, r9 string loc
+    pcl:
+        mov r10, [r9]
+        mov [rdx], r10b
+        inc rdx
+        inc r9
+    dec r8
+    cmp r8, 0
+    jg pcl
 ret
 
 int2String: ;rcx in
@@ -123,7 +444,7 @@ fill: ;rabcdx X,Y,W,H r8 tofill
     push rdx
     push rcx
     mov rcx, printW
-    mov rdx, printgrid
+    mov rdx, printGrid
     call calcAdr
     pop rcx
     mov r11, 0
@@ -175,8 +496,71 @@ printConsoleTz:  ;rdx message pointer;
     mov rbp, rsp
     sub rsp, 10*16
 
-    mov r8, 0
     push rdx
+    call calcTz
+    pop rdx
+
+    call printConsole
+
+    mov rax, 0
+    mov rsp, rbp
+    pop rbp
+ret
+
+drawPlayGrid:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10*16
+
+    mov r10, playGrid
+    add r10, playT
+    dec r10
+
+    mov r8, playH
+    dpgO:
+        mov r9, playW
+        dpgI:
+            mov rax, r9
+            dec rax
+            add rax, rax
+            add rax, playX
+            mov rbx, r8
+            dec rbx
+            add rbx, playY
+
+            mov rcx, printW
+            mov rdx, printGrid
+            call calcAdr
+
+            mov al, [r10]
+            ;sub al, '0'
+
+            mov rcx, colors
+            add cl, al
+            mov al, [rcx]
+
+            mov [rdx], al
+            inc rdx
+            mov [rdx], al
+
+            dec r10
+
+        dec r9
+        cmp r9,0
+        jg dpgI
+
+    dec r8
+    cmp r8,0
+    jg dpgO
+
+    mov rax, 0
+    mov rsp, rbp;
+    pop rbp;
+ret
+
+calcTz: ;in rdx, out r8
+
+    mov r8, 0
     ptzl:
         mov cl, [rdx]
         inc rdx
@@ -185,10 +569,4 @@ printConsoleTz:  ;rdx message pointer;
     jnz ptzl
     dec r8
 
-    pop rdx
-    call printConsole
-
-    mov rax, 0
-    mov rsp, rbp
-    pop rbp
 ret
